23bf3d8ea8358078cece886fb46d3e5b6f69044e
Tyler Petresky <tnpetresky@gmail.com>
2017-01-28 17:59:22 -0500

    Added the Session model

---
+++ src/models/Session.js
@@ -0,0 +1,24 @@
+const config = require('../../config/secrets')
+
+module.exports = (db, DataTypes) => {
+  let Session = db.define('Session', {
+    sid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    sess: {
+      type: DataTypes.JSON,
+      allowNull: false
+    },
+    expire: {
+      type: DataTypes.DATE(6),
+      allowNull: false
+    }
+  }, {
+    tableName: config.sessionTable,
+    timestamps: false
+  }).sync()
+
+  return Session
+}
