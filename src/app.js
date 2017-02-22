f89a94128aa38a6462931ca6e13515e4befc965e
Tyler Petresky <tnpetresky@gmail.com>
2017-02-22 01:11:30 -0500

    Added CORS support to the Express app

--- src/app.js
+++ src/app.js
@@ -38,6 +38,25 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Add headers
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  if (req.method === 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
+})
+
 /**
  * Custom middleware that will instantiate a custom permissions
  * proxy into the request object should a user be logged in.
