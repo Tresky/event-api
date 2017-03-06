0c436cdb34271406fe0b32e3b709d878d42ad3b6
Tyler Petresky <tnpetresky@gmail.com>
2017-03-06 17:02:56 -0500

    Allow for the 'any' use case in the controllerHelpers to allow for 0 or more parameters to be present (as opposed to 1 or more).

--- src/lib/controllerHelpers.js
+++ src/lib/controllerHelpers.js
@@ -17,8 +17,7 @@ module.exports = {
     let intersection = _.intersection(_.keys(params), required)
 
     // We are not receiving what we were expecting, so throw an exception
-    if ((!any && intersection.length !== required.length) ||
-        (any && intersection.length === 0)) {
+    if ((!any && intersection.length !== required.length)) {
       if (_.includes(required, 'universityId') && !_.includes(intersection, 'universityId')) {
         throw new ApiErrors.NoUniversityIdMatch()
       } else {
