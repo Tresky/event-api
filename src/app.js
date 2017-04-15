7f27008294bcdfb3d7627d24b5292b84dc9106e0
Tyler Petresky <tnpetresky@gmail.com>
2017-04-15 10:12:19 -0400

    bug with lowercase Authorization header?

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true)
